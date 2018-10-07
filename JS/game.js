790a540ce7bde3caa926def1d7f81f1b5b8f81ab
Ankit Prakash <43018227+AnkitPrakash687@users.noreply.github.com>
2018-10-07 13:42:35 -0500

    initial commit

---
+++ JS/game.js
@@ -0,0 +1,148 @@
+var originalBoard;
+const player1 = 'X';
+const comPlayer = 'O';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+
+
+function startGame() {
+	document.querySelector("#start-again").style.visibility = "hidden";
+	document.querySelector("table").style.visibility = "visible";
+	document.querySelector("#start").style.visibility = "hidden";
+	document.querySelector(".endgame").style.display = "none";
+	originalBoard = Array.from(Array(9).keys());
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
+
+function turnClick(square) {
+	if (typeof originalBoard[square.target.id] == 'number') {
+		turn(square.target.id, player1)
+		if (!checkWin(originalBoard, player1) && !checkTie()) turn(bestSpot(), comPlayer);
+	}
+}
+
+function turn(squareId, player) {
+	originalBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  if(player == 'X')
+    document.getElementById(squareId).style.backgroundColor = "#ffe60065";
+  else
+   document.getElementById(squareId).style.backgroundColor = "#006eff65";
+  let gameWon = checkWin(originalBoard, player)
+	if (gameWon) gameOver(gameWon)
+}
+
+ function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) =>
+		(e === player) ? a.concat(i) : a, []);
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == player1 ? "blue" : "ff330065";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == player1 ? "You win!" : "You lose.");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector("#start-again").style.visibility = "visible";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return originalBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return minimax(originalBoard, comPlayer).index;
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "#1eff0065";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+	return false;
+}
+
+function minimax(newBoard, player) {
+	var availSpots = emptySquares();
+
+	if (checkWin(newBoard, player1)) {
+		return {score: -10};
+	} else if (checkWin(newBoard, comPlayer)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+
+		if (player == comPlayer) {
+			var result = minimax(newBoard, player1);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, comPlayer);
+			move.score = result.score;
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === comPlayer) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
+}
